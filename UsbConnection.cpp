855343de377b1a67ed6bccc407142b49ea23e033
Andrew Wisner <awisner94@gmail.com>
2015-11-12 21:18:08 -0600

    Got UsbConnection working, but its Fetch() function needs to return a byte array, not a string.

--- UsbConnection.cpp
+++ UsbConnection.cpp
@@ -1,135 +1,87 @@
-#ifndef USB_CONNECTION_H
-#define USB_CONNECTION_H
+#ifndef UsbConnection_H
+#define UsbConnection_H
 
-#include <fcntl.h>
+#include <ftdi.h>
 #include <string>
-#include <termios.h>
-#include <libexplain/read.h>
-#include <libexplain/write.h>
 
 namespace piagnostics {
 
+	/**
+	 * Manages ELM327 via USB.
+	 * Based on code from https://codeseekah.com/2012/02/22/elml327-to-rs232-in-linux.
+	 */
+	
 	class UsbConnection {
 
 		public:
-			UsbConnection(std::string device);
+			UsbConnection();
 			~UsbConnection();
-			std::string Send(std::string data);
+			std::string Fetch(std::string data);
 
 		private:
 			int port;
-			struct termios config;
+			struct ftdi_context context;
 
-		
+			void Send(std::string data);
 	};
 
 	/**
 	 * Initializes a USB connection with a specificed device 
 	 * (e.g., "/dev/ttyUSB0").
-	 *
-	 * This code was based off example code taken from
-	 * https://en.wikibooks.org/wiki/Serial_Programming/terminos
 	 */
 
-	UsbConnection::UsbConnection(std::string device) {		
-		//		if(!isatty(port)) throw ...
+	UsbConnection::UsbConnection() {		
 
-		/* Opens port.
-		 *
-		 * In order:
-		 * 	Opens port for reading and writing
-		 * 	Port never becomes the controlling terminal
-		 * 	Use non-blocking I/O
-		 */
+		int initStatus = ftdi_init(&context);
+		// if(initStatus < 0) throw error
 
-		port = open(device.c_str(), O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK);
-//		port = open(device.c_str(), O_RDWR );	
-
-		// if(port < 0) throw error
-
-		/*
-		 * Turns off input processing.
-		 *
-		 * In order:
-		 * 	Convert break to nullbyte (first two args, I think)
-		 * 	No CR to NL translation
-		 * 	No NL to CR translation
-		 * 	Don't mark parity errors or breaks
-		 *	No Input parity check
-		 *	Don't strip high bit off
-		 *	No XON/XOFF software flow control
-		 */
-
-		//config.c_iflag &= ~(IGNBRK | BRKINT | ICRNL | INLCR
-		//		| PARMRK | INPCK | ISTRIP | IXON);
-
-		/* Output processing. Not sure if it matters that none of these matter */
-		//config.c_oflag = 0;
-
-		/*
-		 * Line processing. Not sure why exactly these matter, but they work in the example.
-		 * 
-		 * In order:
-		 * 	echo off
-		 * 	echo newline off
-		 * 	canonical mode off
-		 * 	extended input processing off
-		 * 	signal chars off
-		 */
-
-		//config.c_lflag &= ~(ECHO | ECHONL | ICANON | IEXTEN | ISIG);
-
-		/*
-		 * Turn off character processing.
-		 *
-		 * In order:
-		 * 	Clear current char size mask
-		 * 	No parity checking
-		 * 	Force 8-bit input
-		 */
-
-		config.c_cflag &= ~(CSIZE | PARENB);
-		config.c_cflag |= CS8;
-
-		config.c_cc[VMIN] = 1;  // One input byte is enough to return from read()
-		config.c_cc[VTIME] = 0;  // Inter-character timer off
-
-		// set speeds
-		int ispeed = cfsetospeed(&config, B115200);
-		int ospeed = cfsetispeed(&config, B115200);
-
-		//		if(ispeed < 0) throw error
-		//		if(ospeed < 0) throw error
-
-		int applyconfig = tcsetattr(port, TCSAFLUSH, &config);
-
-		/*if (applyconfig < 0) {
-			cout <<	explain_tcsetattr(port,TCSAFLUSH,&config);
-		}*/
+		// I think numbers are vendor and product
+		int openStatus = ftdi_usb_open(&context, 0x0403, 0x6001);
+		// if(openStatus < 0) throw error
 		
-		// if(applyconfig < 0) throw error
+		int resetStatus = ftdi_usb_reset(&context);
+		// if(resetStatus < 0) throw error
 		
+		int purgeStatus = ftdi_usb_purge_buffers(&context);
+		// if(purgeStatus < 0) throw error
+		
+		int baudStatus = ftdi_set_baudrate(&context, 115200);
+		// if(baudStatus < 0) throw error
 
-
-
-		char byte[10];
-		int status1 = write(port, "ati", 3);
-		int status2 = read(port, byte, 5);
-	
-		cout << status1 << endl;
-		cout << status2 << endl;
-
-		cout << explain_write(port, "ati", 3) << endl << endl;
-		cout << explain_read(port, byte, 5) << endl << endl;
-		cout << byte << endl;
+		int lineStatus = ftdi_set_line_property2(&context, BITS_8, STOP_BIT_1, NONE, BREAK_OFF);
+		// if(lineStatus < 0) throw error
+		
+		Send("ate0");  // disable echos
+		Send("atsp0");  // automatic protocol detection
 	}
 
 	UsbConnection::~UsbConnection() {
-		close(port);
+		int closeStatus = ftdi_usb_close(&context);
+		// if(closeStatus < 0) throw error
+
+		ftdi_deinit(&context);
 	}
 
-	std::string UsbConnection::Send(std::string data) {
+	std::string UsbConnection::Fetch(std::string data) {
+		const int MAX_LEN = 32;
+		unsigned char temp[MAX_LEN];
 
+		Send(data);
+		ftdi_usb_purge_rx_buffer(&context);
+
+		int len = ftdi_read_data(&context, temp, MAX_LEN);
+		len -= 3;  // removes ">" prompt
+
+		return std::string(reinterpret_cast<const char*>(temp));
+	}
+	
+	void UsbConnection::Send(std::string data) {
+		data += '\r';
+		int writeStatus = ftdi_write_data(&context, (unsigned char*)data.c_str(),
+				sizeof(unsigned char) * data.length());
+		// if(writeStatus < 0) throw error
+		
+		ftdi_usb_purge_tx_buffer(&context);
 	}
 
 }
