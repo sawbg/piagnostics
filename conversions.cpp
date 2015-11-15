3b2314ff3dbb3914199e15c1f156da04e8fef048
Andrew Wisner <awisner94@gmail.com>
2015-11-15 01:36:07 -0600

    Rewrote conversion functions for imperial to metric conversions.

--- conversions.cpp
+++ conversions.cpp
@@ -11,32 +11,11 @@ using namespace std;
 
 namespace piagnostics {
 
-/*	string PidName(Pid pid) {
-		return "";
-	}
-
-	uint8_t to_fahr(uint8_t temp) {
-		return 1.8 * temp + 32;
-	}
-
-	string to_status(uint8_t status) {
-		return ((bool)status ? "On" : "Off");
-	}
-
-	string to_temp(uint8_t temp, Units units) {
-		char unit = 'C';
-
-		if(units == Units::English) {
-			temp = to_fahr(temp);
-			unit = 'F';
-		}
-
-		return to_string(temp) + (char)176 + unit;
-	}*/
-
-	short to_fahr(short temp) { return 1.8 * temp + 32; }
+	short to_celcius(short temp) { return (temp - 32) * 5. / 9.; }
 	
-	short to_mph(short kph) { return (short)(kph * 0.621371); }
+	short to_kph(short mph) { return (short)(mph / 0.621371); }
+
+	short to_kpa(short psi) { return (short)(6.89476 * psi); }
 }
 
 #endif
